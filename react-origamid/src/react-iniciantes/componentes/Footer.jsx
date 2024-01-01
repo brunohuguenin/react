e4841bea3d5528e9a4ad09958dd06a0210b4b024
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-01 13:16:17 -0300

    componentes

---
+++ react-origamid/src/react-iniciantes/componentes/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer>Aqui fica o Footer</footer>;
+};
+
+export default Footer;
---
+++ react-origamid/src/react-iniciantes/componentes/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header>Esse é o Header</header>;
+};
+
+export default Header;
