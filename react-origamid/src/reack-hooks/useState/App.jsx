d198d123ccf302fae409140e418853fccbb78ea7
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-02 14:36:12 -0300

    usestate parte 2

--- react-origamid/src/reack-hooks/useState/App.jsx
+++ react-origamid/src/reack-hooks/useState/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
 
 const App = () => {
-  const [modal, setModal] = React.useState(false);
+  const [contar, setContar] = React.useState(1);
+  const [items, setItems] = React.useState(['Item 1']);
+
+  function handleClick() {
+    setContar((contar) => {
+      // setContar possui um efeito colateral.
+      setItems((items) => [...items, 'Item ' + (contar + 1)]);
+      return contar + 1;
+    });
+    // Tirar o efeito de dentro do setContar irá concertar o erro
+    // setItems((items) => [...items, 'Item ' + (contar + 1)]);
+  }
+
   return (
     <div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+      <button onClick={handleClick}>{contar}</button>
     </div>
   );
 };
