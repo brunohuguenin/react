5277b38347bae2449f79f845ffa4279834c1028a
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-03 13:55:50 -0300

    aula sobre CustomHooks

---
+++ react-origamid/src/reack-hooks/customHooks/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import useLocalStorage from './useLocalStorage';
+import useFetch from './useFetch';
+
+const App = () => {
+  const [produto, setProduto] = useLocalStorage('produto', '');
+  const { request, data, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fecthData() {
+      const { response, json } = await request(
+        'https://ranekapi.origamid.dev/json/api/produto/',
+      );
+    }
+    fecthData();
+  }, [request]);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data)
+    return (
+      <div>
+        <p>Produto preferido: {produto}</p>
+        <button onClick={handleClick}>notebook</button>
+        <button onClick={handleClick}>smartphone</button>
+
+        {data.map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+};
+
+export default App;
