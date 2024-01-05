8afae88d3feb03779d566f478390e6f25372854a
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-05 16:55:01 -0300

    componente input

---
+++ react-origamid/src/formularios/Componentes-Form/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Input = ({ id, label, setValue, value, ...props }) => {
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type="text"
+        name={id}
+        id={id}
+        value={value}
+        onChange={({ target }) => setValue(target.value)}
+        {...props}
+      />
+    </>
+  );
+};
+
+export default Input;
