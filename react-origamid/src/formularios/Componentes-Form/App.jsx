de5f65c15fea77e0db9d8de8bfbcb3ddb412d74f
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-05 17:33:20 -0300

    componente radio

--- react-origamid/src/formularios/Componentes-Form/App.jsx
+++ react-origamid/src/formularios/Componentes-Form/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Input from './Input';
 import Select from './Select';
+import Radio from './Radio';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
+  const [carro, setCarro] = React.useState('');
 
   return (
     <form>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+
+      <h2>Carros</h2>
+      <Radio
+        options={['Mustang', 'Lamborghini', 'Dodge Challenger']}
+        value={carro}
+        setValue={setCarro}
+      />
+
       <Select
         options={['Smartphone', 'Tablet']}
         value={produto}
