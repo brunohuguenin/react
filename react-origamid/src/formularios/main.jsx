7e2268bd6754cfcf2c43ca27e9eca15ef9f2fe2c
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-05 13:31:29 -0300

    aula sobre input radio

--- react-origamid/src/formularios/main.jsx
+++ react-origamid/src/formularios/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './select/App';
+import App from './radio/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
