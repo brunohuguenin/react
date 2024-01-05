b0cccc300ef73c3872b06e422a55991a6de18af2
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-05 17:10:59 -0300

    componente select

--- react-origamid/src/formularios/Componentes-Form/App.jsx
+++ react-origamid/src/formularios/Componentes-Form/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Input from './Input';
+import Select from './Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
+  const [produto, setProduto] = React.useState('');
 
   return (
     <form>
+      <Select
+        options={['Smartphone', 'Tablet']}
+        value={produto}
+        setValue={setProduto}
+      />
       <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
       <button>Enviar</button>
