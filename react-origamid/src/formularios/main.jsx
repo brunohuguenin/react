975b92725da8a1474fd9b2c5dd5c9371d490ffca
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-05 13:13:12 -0300

    aula sobre textearea

--- react-origamid/src/formularios/main.jsx
+++ react-origamid/src/formularios/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './input/ExeApp';
+import App from './textarea/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ react-origamid/src/formularios/textarea/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  const [textarea, setTextarea] = React.useState('');
+
+  return (
+    <form>
+      <textarea
+        value={textarea}
+        onChange={({ target }) => setTextarea(target.value)}
+        rows="10"
+      ></textarea>
+    </form>
+  );
+};
+
+export default App;
